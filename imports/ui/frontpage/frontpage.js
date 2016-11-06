668639b02805f11339ed301899192b1b6a9ba868
Ricardo Galdámez <ricardo.j.galdamez@gmail.com>
2016-11-06 13:12:53 -0600

    adding some packages, adding admin

--- imports/ui/frontpage/frontpage.js
+++ imports/ui/frontpage/frontpage.js
@@ -1,1 +1,30 @@
 import "./frontpage.html";
+
+Template.frontpage.events({
+
+/*
+"click #add_user" (event){
+
+    var Profile = {
+        firstname: 'admin',
+        lastname: 'admin',
+      }
+      var User = {
+        username: 'admin',
+        email: 'admin@admin.com',
+        password: 'admin',
+        profile: Profile
+      }
+      Accounts.createUser(User, function(err){
+        if(err){
+          Materialize.toast('El usuario ya existe', 4000);
+      
+        }else{
+          Materialize.toast('Registrado Correctamente', 4000);
+        }
+      });
+}
+*/
+
+
+});
