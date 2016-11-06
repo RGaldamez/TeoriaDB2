5ae34bc181895bd2d002f7da19ce9ef18d02f718
Ricardo Galdámez <ricardo.j.galdamez@gmail.com>
2016-11-06 12:17:50 -0600

    Adding empty ui

---
+++ imports/routes/router.js
@@ -0,0 +1,23 @@
+import {Router} from "meteor/iron:router";
+
+import "../ui/frontpage/frontpage.js";
+import "../ui/home/home.js";
+import "../ui/homeAdmin/homeAdmin.js";
+import "../ui/login/login.js";
+
+
+Router.route('/', function(){
+    this.render("frontpage");
+});
+
+Router.route('home',function(){
+    this.render("home");
+});
+
+Router.route('homeAdmin', function(){
+    this.render("homeAdmin");
+});
+
+Router.route('login', function(){
+    this.render("login");
+})
